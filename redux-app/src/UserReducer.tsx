1172c0027562f942461d0812806078f634a82b0e
cynthia-isutsa <isutsacynthia@gmail.com>
2024-05-11 22:19:36 +0300

    redux setup

---
+++ redux-app/src/UserReducer.tsx
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const userSlice = createSlice({
+    name: "users", 
+    initialState: {},
+    reducers: {
+        
+    }
+})
+
+const UserReducer = () => {
+  return (
+    <div>UserReducer</div>
+  )
+}
+
+export default UserReducer
